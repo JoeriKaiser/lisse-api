7cac6821cd36cb119815b2920c852d5e4e4e40b7
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-07-28 00:37:42 +0200

    Finished first post route config

--- src/routes/scanner/scannerRoutes.ts
+++ src/routes/scanner/scannerRoutes.ts
@@ -1,12 +1,22 @@
+import { bodyParser } from "@koa/bodyparser";
 import Router from "@koa/router";
 import type { Context } from "koa";
 
 const router = new Router();
 
+router.use(bodyParser());
+
 router.prefix("/scanner");
 
 router.get("/", async (ctx: Context) => {
   ctx.body = { message: "Hello Scanner World!" };
+});
+
+router.post("/scan", async (ctx: Context) => {
+  const request = ctx.request;
+  console.log(request.body);
+
+  ctx.body = { message: `Successfully scanned ${request.body.name}` };
 });
 
 export default router;
