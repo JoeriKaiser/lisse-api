ec906c7d678d7f7d7e237aadfc84eb0110f44739
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-08-01 20:54:30 +0200

    Add session config and more cors config with env variables

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,35 @@
 import Koa from 'koa';
+import session from 'koa-session';
 import router from './routes/routes';
 import cors from '@koa/cors';
+import passport from './middleware/passport';
+import { config } from 'dotenv';
+
+// TODO Make dynamic env variables available
+config({ path: '.env.local' });
 
 const app = new Koa();
 
-app.use(cors());
+app.keys = [process.env.SESSION_KEY!];
+const CONFIG = {
+  key: 'koa:sess',
+  maxAge: 86400000, // 1 day in milliseconds
+  autoCommit: true,
+  overwrite: true,
+  httpOnly: true,
+  signed: true,
+  rolling: false,
+  renew: false,
+};
+app.use(session(CONFIG, app));
+app.use(
+  cors({
+    origin: 'http://localhost:5173', //TODO change this to the correct origin
+    credentials: true,
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
