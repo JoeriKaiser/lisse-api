0243de40e72700b3951cf675e6d452411432bef7
JoeriKaiser <joeri.kaiser@hotmail.com>
2024-10-25 17:41:16 +0200

    feat(scan): Add get route for user scns using parameter

--- src/routes/scanner/scannerRoutes.ts
+++ src/routes/scanner/scannerRoutes.ts
@@ -3,6 +3,8 @@ import Router from '@koa/router';
 import type { Context } from 'koa';
 import db from '../../db/datasource';
 import { scans } from '../../db/schema';
+import { eq } from 'drizzle-orm';
+import { parse } from 'dotenv';
 
 const router = new Router();
 
@@ -14,8 +16,26 @@ router.get('/', async (ctx: Context) => {
   ctx.body = { message: 'Hello Scanner World!' };
 });
 
+router.get('/scans/:userId', async (ctx: Context) => {
+  const { userId } = ctx.params;
+
+  if (!userId) {
+    ctx.throw(400, 'User ID is required');
+  }
+
+  console.log(userId);
+  try {
+    ctx.body = await db
+      .select()
+      .from(scans)
+      .where(eq(scans.userId, parseInt(userId)))
+      .execute();
+  } catch (error) {
+    ctx.throw(500, error as Error);
+  }
+});
+
 router.post('/scan', async (ctx: Context) => {
-  // TODO type this
   const body = ctx.request.body;
 
   if (!body.barcode) {
